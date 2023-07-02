2c45cecf38a51715a52dc730db1af09990ad4afe
n-sakuma39 <naoto.sakuma39@gmail.com>
2023-07-02 19:43:58 +0900

    修正

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Link from "next/link"
+import Link from "next/link";
+import Header from "@/components/Header";
 
-export default function home () {
-  return(
+export default function home() {
+  return (
     <div>
+      <Header />
       <h1>TOPページ</h1>
-      <Link href="/blog/">
-        ブログ一覧へ
-      </Link>
+      <Link href="/blog/">ブログ一覧へ</Link>
     </div>
-  )
+  );
 }
