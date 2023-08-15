ecb215b321cc64a21bdcf30ab83d636be648e43f
n-sakuma39 <naoto.sakuma39@gmail.com>
2023-08-15 19:21:11 +0900

    TOP上部へのボタン追加（8/15）

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,24 @@
+import React from "react";
+import { PiArrowLineUp } from "react-icons/pi";
+import { animateScroll as scroll } from "react-scroll";
 import styles from "@/components/Footer/Footer.module.scss";
 
 export const Footer = () => {
+  const scrollToTop = () => {
+    scroll.scrollToTop({
+      duration: 500,
+      smooth: "easeInOutQuart",
+    });
+  };
+
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>&copy; NAOTO SAKUMA</div>
+      <div className={styles.scrollButton} onClick={scrollToTop}>
+        <PiArrowLineUp size={40} />
+      </div>
+      <div className={styles.inner}>
+        <div className={styles.copyright}>&copy; NAOTO SAKUMA</div>
+      </div>
     </footer>
   );
 };
